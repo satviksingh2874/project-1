b34bdcb3c3f922e19886134726d1a0d8783d0362
Bikramjeet <bikramjeetsingh2609@gmail.com>
2024-05-10 12:57:10 +0530

    bikki

--- home.py
+++ home.py
@@ -7,10 +7,22 @@ app=Flask(__name__)
 def page2():
     return render_template("page2.html")
 
+@app.route("/page3")
+def page3():
+    return render_template('page3.html')
+
 @app.route("/")
 @app.route("/home")
 def home():
     return render_template("page1.html")
+
+@app.route('/page4')
+def page4():
+    return render_template("page4.html")
+
+@app.route("/page5")
+def page5():
+    return render_template("page5.html")
 
 if __name__=="__main__":
     app.run(debug=True)
